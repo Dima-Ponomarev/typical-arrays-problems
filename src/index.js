8e709a222a6dbe69f5f53d28297a04a988295d8f
Dima Ponomarev <kikkyow18@gmail.com>
2021-02-19 15:25:58 +0400

    feat: implement max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined){
+    return 0;
+  } else if (array.length === 0){
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined){
+    return 0;
+  } else if (array.length === 0){
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  if (array === undefined){
+    return 0;
+  } else if (array.length === 0){
+    return 0;
+  } else {
+    return array.reduce((sum, curValue) => sum + curValue) / array.length;
+  }
 }
